e08da139fe31bb7e42a6f91dfd9850e40d588500
Pedro Cruz <60475001+pedroc07@users.noreply.github.com>
2025-05-19 13:09:32 -0300

    Update ProblemaE.m

--- ProblemaE.m
+++ ProblemaE.m
@@ -22,6 +22,18 @@ function [mX] = AproximacaoSeidel(mA, x0)
   mX(3) = mX(3)/mA(3,3);
 end
 
+function [mX] = AproximacaoSOR(mA, x0, w)
+  mX(1) = (mA(1,2) * -1 * x0(2)) + (mA(1,3) * -1 * x0(3)) + mA(1,4);
+  mX(1) = mX(1)/mA(1,1);
+  mX(1) = mX(1) * w + (1 - w) * x0(1);
+  mX(2) = (mA(2, 1) * -1 * mX(1)) + (mA(2,3) * -1 * x0(3)) + mA(2,4);
+  mX(2) = mX(2)/mA(2,2);
+  mX(2) = mX(2) * w + (1 - w) * x0(2);
+  mX(3) = (mA(3,1) * -1 * mX(1)) + (mA(3,2) * -1 * mX(2)) + mA(3,4);
+  mX(3) = mX(3)/mA(3,3);
+  mX(3) = mX(3) * w + (1 - w) * x0(3);
+end
+
 function [dr] = Calcula_dr(mX1, mX0)
     dr = max(abs(mX1 .- mX0)) / max(abs(mX1));
 end
@@ -94,11 +106,28 @@ function [xi] = GaussJacobi(mA, epsilon)
   xi = x0;
   k = 0;
   converg = DiagDominante(mA, 3);
-  if converg = 0
+  if converg = 1
     while epsilon < dr
       aux = xi;
       k = k + 1
       xi = AproximacaoJacobi(mA, xi);
+      dr = Calcula_dr(xi, aux);
+    endwhile
+  endif
+end
+
+% IMPLEMENTACAO DO METODO SOR
+function [xi] = SOR(mA, epsilon)
+  dr = 1;
+  x0 = [0, 0, 0];
+  xi = x0;
+  k = 0;
+  converg = Sassenfeld(mA, 3);
+  if converg = 1
+    while epsilon < dr
+      aux = xi;
+      k = k + 1
+      xi = AproximacaoSOR(mA, xi);
       dr = Calcula_dr(xi, aux);
     endwhile
   endif
@@ -109,3 +138,4 @@ mA = [-132, 22, 0, -1000; 5, -27, 7, -2000; 117, 0, -22, 110];
 epsilon = 0.01;
 mX = GaussSeidel(mA, epsilon)
 mX = GaussJacobi(mA, epsilon)
+mX = SOR(mA, epsilon)
